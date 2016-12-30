5675ca8e1e887d78f67e399c81d050e981d99456
shuchao <645904877@qq.com>
2016-12-30 10:00:46 +0800

    fix bug

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 
         UserModel := Connector.DB("vpn").C("users")
 
-        if UserModel.Find(bson.M{"Port": int32(8388)}).All(&Users) == nil {
+        if UserModel.Find(bson.M{"Port": int32(8388)}).Select(bson.M{"Port": 1}).All(&Users) == nil {
             fmt.Println(Users)
             for _, User := range Users {
                 Ports = append(Ports, User.Port)
