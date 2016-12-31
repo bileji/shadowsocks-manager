5541e270afb1494528a79be0bcc7895674d2b61b
shuchao <645904877@qq.com>
2016-12-31 21:50:56 +0800

    fix bug

--- service/web.go
+++ service/web.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "gopkg.in/mgo.v2"
     "encoding/json"
+    "io/ioutil"
 )
 
 type Web struct {
@@ -25,33 +26,33 @@ type AddUserParams  struct {
 
 // todo 添加用户
 func addUser(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-    fmt.Println(r.PostFormValue("port"), r.Method, r.PostForm, r.Body)
-    if r.Method == "POST" {
-        var Params AddUserParams
-        fmt.Println(r.Form)
-        Args, err := json.Marshal(r.Form)
-        if err == nil {
-            if json.Unmarshal(Args, &Params) == nil {
-                fmt.Println(Params)
-            }
-        }
-    }
-
-    //fmt.Println(r.PostFormValue("port"), r.Method, r.Form)
-    //fmt.Printf("type: %T, value: %s", r.PostFormValue("port"), r.PostFormValue("port"))
     //r.ParseForm()
+    //fmt.Println(r.PostFormValue("port"), r.Method, r.PostForm, r.Body)
     //if r.Method == "POST" {
-    //    result, _ := ioutil.ReadAll(r.Body)
-    //    r.Body.Close()
-    //
-    //    var Params AddU
-    //    json.Unmarshal([]byte(result), &Params)
-    //
-    //    fmt.Println(Params)
-    //} else {
-    //
+    //    var Params AddUserParams
+    //    fmt.Println(r.Form)
+    //    Args, err := json.Marshal(r.Form)
+    //    if err == nil {
+    //        if json.Unmarshal(Args, &Params) == nil {
+    //            fmt.Println(Params)
+    //        }
+    //    }
     //}
+
+    fmt.Println(r.PostFormValue("port"), r.Method, r.Form)
+    fmt.Printf("type: %T, value: %s", r.PostFormValue("port"), r.PostFormValue("port"))
+    r.ParseForm()
+    if r.Method == "POST" {
+        result, _ := ioutil.ReadAll(r.Body)
+        r.Body.Close()
+
+        var Params AddUserParams
+        json.Unmarshal([]byte(result), &Params)
+
+        fmt.Println(Params)
+    } else {
+
+    }
 }
 
 // todo 移除用户
