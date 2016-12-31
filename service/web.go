c565c0a06a910f91f0c9393a94d9bfad46816516
shuchao <645904877@qq.com>
2016-12-31 20:56:29 +0800

    fix bug

--- service/web.go
+++ service/web.go
@@ -25,7 +25,7 @@ type AddU struct {
 
 // todo 添加用户
 func addUser(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.PostFormValue("port"))
+    fmt.Println(r.PostFormValue("port"), r.Method)
     fmt.Printf("type: %T, value: %s", r.PostFormValue("port"), r.PostFormValue("port"))
     r.ParseForm()
     if r.Method == "POST" {
